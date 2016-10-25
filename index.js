0d1cdd36d0b9953f5ce9dd5c0e5dfa61716df0ec
Zakaria Ridouh <zridouh@live.com>
2016-10-24 23:12:23 -0700

    :boom: start work on index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use strict';
 
+const url = require('url');
 const got = require('got');
 const cheerio = require('cheerio');
 
-// @todo
+function getUserUrl(username = '') {
+  return `https://github.com/${username}`
+}
+
+function getUsername(userUrl = 'https://github.com/sindresorhus') {
+  
+  // @todo fix problems caused by using http protocol vs https
+}
